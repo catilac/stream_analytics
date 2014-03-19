27a9f12468c2ee5c5293a261b8de6d45aabf979d
Chirag Davé <chirage@gmail.com>
2014-03-18 23:56:08 -0700

    report number of connections at connect/disconnect

--- server/server.py
+++ server/server.py
@@ -5,12 +5,24 @@ import threading
 
 from top_hashtags import TopHashtags
 
+top_hashtags = TopHashtags()
+threading.Thread(target=top_hashtags.start).start()
+
 class StatServer(Protocol):
-    def __init__(self, top_hashtags):
+    def __init__(self, factory, top_hashtags):
+        self.factory = factory
         self._top_hashtags = top_hashtags
 
     def get_top(self, num):
         return self._top_hashtags.get_top_n(num)
+
+    def connectionMade(self):
+        self.factory.numProtocols = self.factory.numProtocols+1
+        print "CONNECT Num Connections is now: %d" % self.factory.numProtocols
+
+    def connectionLost(self, reason):
+        self.factory.numProtocols = self.factory.numProtocols-1
+        print "DISCONNECT Num Connections is now: %d" % self.factory.numProtocols
 
     def dataReceived(self, data):
         num = 15
@@ -22,13 +34,15 @@ class StatServer(Protocol):
 
         top_hashtags = str(self.get_top(num))
         self.transport.write(top_hashtags)
+        self.transport.loseConnection()
 
 class StatFactory(Factory):
+    def __init__(self):
+        self.numProtocols = 0
+
     def buildProtocol(self, addr):
         """ Start TopHash and pass it into protocol """
-        self.top_hashtags = TopHashtags()
-        threading.Thread(target=self.top_hashtags.start).start()
-        return StatServer(self.top_hashtags)
+        return StatServer(self, top_hashtags)
 
 def main():
     endpoint = TCP4ServerEndpoint(reactor, 8000)
