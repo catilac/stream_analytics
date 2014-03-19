6a0c3545fa572c1e7c4990448bf8cdc8988e17af
Chirag Davé <chirage@gmail.com>
2014-03-18 21:48:55 -0700

    initial sketch for requesting data

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,3 +1,44 @@
+var RealTimeHashTags = function(n) {
+    if (n) {
+        this.n = n;
+    }
+    this.n = 15;
+
+    /* start()
+     * receive updates from server
+     */
+    this.start = function() {
+        this._getUpdates();
+    };
+
+    this._topUrl = function() {
+        return "http://localhost:8000/top/"+(this.n || 10);
+    };
+
+    this._getUpdates = function() {
+        var topUrl = this._topUrl();
+        $.ajax({
+            url: topUrl,
+        }).done(this._onReceive);
+    };
+
+    this._onReceive = function(data) {
+        console.log("Received: ", data);
+        this._getUpdates();
+        return;
+    };
+
+    this.setOnReceive = function(func) {
+        if (func && typeof(func) === "function") {
+            this._onReceive = func;
+        }
+    };
+};
+
 $(function() {
-    alert("Loaded");
+    rtht = new RealTimeHashTags();
+    rtht.setOnReceive(function(data) {
+        console.log(data);
+    });
+    rtht.start();
 });
