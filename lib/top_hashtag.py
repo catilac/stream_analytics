e6d49be8d8ac37a48704b088b18e3f5e1579a81f
Chirag Davé <chirage@gmail.com>
2014-03-19 01:39:22 -0700

    little better socket handling code

--- lib/top_hashtag.py
+++ lib/top_hashtag.py
@@ -3,24 +3,46 @@ from ast import literal_eval
 
 class TopHashtagClient(object):
     def __init__(self):
-        self.ip = '127.0.0.1'
-        self.port = 8000
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((self.ip, self.port))
+        self._ip = '127.0.0.1'
+        self._port = 8000
+        self._sock = None
 
     def __del__(self):
-        self.sock.close()
+        self.disconnect()
+
+    def connect(self):
+        if self._sock:
+            return
+        sock = self._connect()
+        self._sock = sock
+
+    def _connect(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self._ip, self._port))
+        return sock
+
+    def disconnect(self):
+        if self._sock is None:
+            return
+        try:
+            self._sock.shutdown(socket.SHUT_RDWR)
+            self._sock.close()
+        except:
+            pass
+        self._sock = None
 
     def get_top_n(self, n):
-        self.sock.send(str(n))
-        data = self.sock.recv(4096)
-        return literal_eval(data)
+        if not self._sock:
+            self.connect()
+        try:
+            self._sock.send(str(n))
+            data = self._sock.recv(4096)
+            return literal_eval(data)
+        except:
+            self.disconnect()
+            return None
 
-
-def main():
+if __name__ == '__main__':
     client = TopHashtagClient()
     while 1:
         print client.get_top_n(5)
-
-if __name__ == '__main__':
-    main()
