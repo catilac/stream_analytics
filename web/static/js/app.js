95882a41df018d9b988c1c438d62d55efc698916
Chirag Davé <chirage@gmail.com>
2014-03-19 22:46:41 -0700

    drawing graph with svg TODO: animate updates

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -45,7 +45,44 @@ var RealTimeHashTags = function(n) {
     };
 };
 
+function processD3Factory() {
+    var redraw = false;
+    return function(data) {
+        console.log(data);
+        var _data = JSON.parse(data);
+
+        var width = 420,
+            barHeight = 20;
+
+        var x = d3.scale.linear()
+            .domain([0, d3.max(_data, function(d){ return d[1]; })])
+            .range([0, width]);
+
+        var chart = d3.select('.chart')
+            .attr("width", width)
+            .attr("height", barHeight * _data.length);
+
+        var bar = chart.selectAll("g")
+            .data(_data)
+          .enter().append("g")
+            .attr("transform", function(d,i) {
+                return "translate(0," + i * barHeight +")";
+            });
+
+        bar.append("rect")
+            .attr("width", function(d) { return x(d[1]); })
+            .attr("height", barHeight - 1);
+
+        bar.append("text")
+            .attr("x", function(d) { return x(d[1]) - 3; })
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em")
+            .text(function(d) { return d[0] + "(" + d[1] + ")" });
+    };
+}
+
 $(function() {
     rtht = new RealTimeHashTags();
+    rtht.setOnReceive(processD3Factory());
     rtht.start();
 });
